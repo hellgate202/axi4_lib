bcd7b107267c4552cd0c0db800fd97f4f1033104
Leonid Ivanov <l.ivanov@metrotek.ru>
2020-04-21 13:09:10 +0300

    added python scripts for img converting

---
+++ scripts/display_rgb_hex.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+import sys
+
+px_width = 10
+
+value_mult = 2 ** ( px_width - 8 )
+
+img_path = sys.argv[1]
+x = int( sys.argv[2] )
+y = int( sys.argv[3] )
+
+d_img = np.zeros( ( y, x, 3 ), np.uint8 )
+
+img = open( img_path )
+
+print( "Reading output image..." )
+for i in range( y ):
+  for j in range( x ):
+    l = d.readline().strip()
+    d_img[i][j][0] = ( int( ( "0b" + l ), 2 ) / value_mult );
+    l = d.readline().strip()
+    d_img[i][j][1] = ( int( ( "0b" + l ), 2 ) / value_mult );
+    l = d.readline().strip()
+    d_img[i][j][2] = ( int( ( "0b" + l ), 2 ) / value_mult );
+
+cv2.imshow( "img", d_img )
+cv2.waitKey( 0 )
+cv2.destroyAllWindows()
---
+++ scripts/rgb_img2bayer_hex.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2
+import sys
+
+output_path = "./img.hex"
+px_width    = 10
+
+value_mult = 2 ** ( px_width - 8 )
+
+def convert2bayer( img ):
+  img_bay = np.zeros( ( img.shape[0], img.shape[1] ), dtype = np.uint8 )
+  for y in range( img.shape[0] ):
+    for x in range( img.shape[1] ):
+      if ( y % 2 ) == 0:
+        if ( x % 2 ) == 0:
+          img_bay[y][x] = img[y][x][2]
+        else:
+          img_bay[y][x] = img[y][x][1]
+      else:
+        if ( x % 2 ) == 0:
+          img_bay[y][x] = img[y][x][1]
+        else:
+          img_bay[y][x] = img[y][x][0]
+  return img_bay
+
+img = cv2.imread( sys.argv[1] )
+new_x = int( sys.argv[2] )
+new_y = int( sys.argv[3] )
+
+print( "Resizing image to %0d x %0d" % ( new_x, new_y ) )
+img = cv2.resize( img, ( new_x, new_y ) )
+
+print( "Converting to Bayer pattern" )
+bayer = convert2bayer( img )
+
+print( "Creating sample hex file" )
+with open( output_path, "w+" ) as f:
+  for y in range( bayer.shape[0] ):
+    for x in range( bayer.shape[1] ):
+      f.write( hex( bayer[y][x] * value_mult )[2 :]+"\n" )
